ac8a016e238e1336c5cb8664ddee046c6dd001e7
rzvpop <32769990+rzvpop@users.noreply.github.com>
2018-03-05 16:26:22 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,19 +2,11 @@
 
 int main()
 {
-    /*int n;
-    std::cin >> n;*/
-
     Graph::Graph g(5);
 
-    g.AddEdge(Graph::Edge(1, 3, 0));
-    g.AddEdge(Graph::Edge(2, 0, 5));
-    g.AddEdge(Graph::Edge(2, 4, 2));
+    g.ReadFromFile("graph_ex.txt");
 
-    g.RemoveEdge(Graph::Edge(2, 0, 5));
 
-    g.Print();
-    std::cout << g.GetNrVertices() << " " << g.GetNrEdges() << '\n';
 
 return 0;
 }
